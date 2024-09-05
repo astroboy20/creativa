e2acbbcfe8055131fb9a6c151fa2f016fefc1ef0
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-09-05 21:46:32 +0100

    feat: error fixed

---
+++ src/containers/home/container.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Hero } from './hero'
+import { Meet } from './meet'
+
+const Container = () => {
+  return (
+    <>
+    <Hero/>
+    <Meet/>
+    </>
+  )
+}
+
+export  {Container}
